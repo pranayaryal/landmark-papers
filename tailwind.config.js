cb9674fcb8d9f01163ef1fdaa8e2b925d4556a74
pranay <drpranayaryal@gmail.com>
2020-06-19 23:45:07 -0400

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'back-black': '#071a52',
+        'menu-color': '#17b978'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
